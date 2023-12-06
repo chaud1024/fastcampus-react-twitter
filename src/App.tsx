d3a2fe17b7f3f393cd1265acea81d2bfe6b065d8
chaud1024 <chaud1024@gmail.com>
2023-12-06 22:10:10 +0900

    add toastContainer, onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import Layout from "components/Layout";
 import Router from "components/Router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "firebaseApp";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
 
-function App() {
+import "react-toastify/dist/ReactToastify.css";
+
+function App({}) {
+  const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth.currentUser,
+  );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <Layout>
-      <Router />
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : "Loading"}
     </Layout>
   );
 }
