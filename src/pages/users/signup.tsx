4a2ca942cb17c6caf249d25a8050b252220762ae
chaud1024 <chaud1024@gmail.com>
2023-12-06 22:08:13 +0900

    add signupform

--- src/pages/users/signup.tsx
+++ src/pages/users/signup.tsx
@@ -1,7 +1,9 @@
+import SignupForm from "components/users/SignupForm";
+
 export default function SignupPage() {
   return (
     <div>
-      <h1>SignupPage</h1>
+      <SignupForm />
     </div>
   );
 }
