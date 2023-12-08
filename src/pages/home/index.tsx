b46e5dd0f8dd85f2c910a7dc3bd2aa25d57cee7a
chaud1024 <chaud1024@gmail.com>
2023-12-09 00:49:14 +0900

    update home top by wrap

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -53,10 +53,12 @@ const posts: PostProps[] = [
 export default function Homepage() {
   return (
     <div className="home">
-      <div className="home__title">Home</div>
-      <div className="home__tabs">
-        <div className="home__tab home__tab--active">For You</div>
-        <div className="home__tab home__tab">Following</div>
+      <div className="home__top">
+        <div className="home__title">Home</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab home__tab">Following</div>
+        </div>
       </div>
       <PostForm />
       <div className="post">
