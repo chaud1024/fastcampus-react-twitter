a822e77f477c4799eacf1dec047c63e1d2bbb546
chaud1024 <chaud1024@gmail.com>
2023-12-09 16:45:17 +0900

    add PostEditForm ui및 로직 추가

---
+++ src/components/posts/PostEditForm.tsx
@@ -0,0 +1,83 @@
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import { useCallback, useEffect, useState } from "react";
+import { FiImage } from "react-icons/fi";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+
+export default function PostEditForm() {
+  const params = useParams();
+  const [post, setPost] = useState<PostProps | null>(null);
+  const [content, setContent] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleFileUpload = () => {};
+
+  const getPost = useCallback(async () => {
+    if (params.id) {
+      const docRef = doc(db, "posts", params.id);
+      const docSnap = await getDoc(docRef);
+      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+      setContent(docSnap?.data()?.content);
+    }
+  }, [params.id]);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      if (post) {
+        const postRef = doc(db, "posts", post?.id);
+        await updateDoc(postRef, {
+          content: content,
+        });
+        navigate(`/posts/${post.id}`);
+        toast.success("게시글을 수정했습니다.");
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "content") {
+      setContent(value);
+    }
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      getPost();
+    }
+  }, [getPost, params.id]);
+
+  return (
+    <form className="post-form" onSubmit={handleSubmit}>
+      <textarea
+        className="post-form__textarea"
+        name="content"
+        id="content"
+        required
+        placeholder="What is happening?"
+        onChange={handleChange}
+        value={content}
+      />
+      <div className="post-form__submit-area">
+        <label htmlFor="file-input" className="post-form__file">
+          <FiImage className="post-form__file-icon" />
+        </label>
+        <input
+          type="file"
+          name="file-input"
+          accept="image/*"
+          onChange={handleFileUpload}
+          className="hidden"
+        />
+        <input type="submit" value="Edit" className="post-form__submit-btn" />
+      </div>
+    </form>
+  );
+}
