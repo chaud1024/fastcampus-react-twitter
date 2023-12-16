85f773ff6175bdbe4a582ac546046c27ffa25f60
chaud1024 <chaud1024@gmail.com>
2023-12-16 14:56:41 +0900

    update profile edit page

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -1,7 +1,134 @@
+import PostHeader from "components/posts/PostHeader";
+import AuthContext from "context/AuthContext";
+import { updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import { storage } from "firebaseApp";
+import { useContext, useEffect, useState } from "react";
+import { FiImage } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { v4 as uuidv4 } from "uuid";
+
 export default function ProfileEdit() {
+  const { user } = useContext(AuthContext);
+  const [displayName, setDisplayName] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+
+    setDisplayName(value);
+  };
+
+  const handleSubmit = async (e: any) => {
+    let key = `${user?.uid}/${uuidv4()}`;
+    const storageRef = ref(storage, key);
+    let newImageUrl = null;
+
+    e.preventDefault();
+
+    try {
+      // 기존 이미지 삭제의 경우 만들어둔 프로필 디폴트 이미지 제공
+
+      // 새로운 이미지 업로드
+      if (imageUrl) {
+        const data = await uploadString(storageRef, imageUrl, "data_url");
+        newImageUrl = await getDownloadURL(data?.ref);
+      }
+      // uploadProfile 호출
+      if (user) {
+        await updateProfile(user, {
+          displayName: displayName || "",
+          photoURL: newImageUrl || "",
+        })
+          .then(() => {
+            toast.success("프로필이 업데이트 되었습니다.");
+            navigate("/profile");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  };
+
+  const handleFileUpload = (e: any) => {
+    const {
+      target: { files },
+    } = e;
+    const file = files?.[0];
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = (e: any) => {
+      const { result } = e.currentTarget;
+      setImageUrl(result);
+    };
+  };
+  const handleDeleteImage = () => {
+    setImageUrl(null);
+  };
+
+  useEffect(() => {
+    if (user?.photoURL) {
+      setImageUrl(user.photoURL);
+    }
+    if (user?.displayName) {
+      setDisplayName(user?.displayName);
+    }
+  }, [user]);
+
   return (
-    <div>
-      <h1>ProfileEdit</h1>
+    <div className="post">
+      <PostHeader />
+      <form className="post-form" onSubmit={handleSubmit}>
+        <div className="post-form__profile">
+          <input
+            type="text"
+            name="displayName"
+            id="displayName"
+            className="post-form__input"
+            placeholder="Name"
+            onChange={handleChange}
+            value={displayName}
+          />
+        </div>
+        {imageUrl && (
+          <div className="post-form__attachment">
+            <img src={imageUrl} alt="attachment" width={100} height={100} />
+            <button
+              className="post-form__clear-btn"
+              type="button"
+              onClick={handleDeleteImage}>
+              Clear
+            </button>
+          </div>
+        )}
+        <div className="post-form__submit-area">
+          <div className="post-form__image-area">
+            <label htmlFor="file-input" className="post-form__file">
+              <FiImage className="post-form__file-icon" />
+            </label>
+            <input
+              type="file"
+              name="file-input"
+              id="file-input"
+              accept="image/*"
+              onChange={handleFileUpload}
+              className="hidden"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Edit profile"
+            className="post-form__submit-btn"
+          />
+        </div>
+      </form>
     </div>
   );
 }
