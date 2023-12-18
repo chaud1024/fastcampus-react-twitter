9d82eff94c303deaeb9a33339f5edee3dca0706a
chaud1024 <chaud1024@gmail.com>
2023-12-18 09:26:31 +0900

    create language TRANSLATIONS constants

---
+++ src/constants/language.ts
@@ -0,0 +1,29 @@
+const TRANSLATIONS = {
+  // MENU
+  MENU_HOME: {
+    ko: "홈",
+    en: "Home",
+  },
+  MENU_PROFILE: {
+    ko: "프로필",
+    en: "Profile",
+  },
+  MENU_SEARCH: {
+    ko: "검색",
+    en: "Search",
+  },
+  MENU_NOTI: {
+    ko: "알림",
+    en: "Notifications",
+  },
+  MENU_LOGOUT: {
+    ko: "로그아웃",
+    en: "Logout",
+  },
+  MENU_LOGIN: {
+    ko: "로그인",
+    en: "Login",
+  },
+};
+
+export default TRANSLATIONS;
